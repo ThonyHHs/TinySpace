5fd269dd6467b682dda9c4a171ff95e738b7f54d
Anthony Herbert <herbert.anthony068@hotmail.com>
2025-01-23 18:01:33 -0300

    a

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include "game.h"
 
 int main() {
-    std::cout << sizeof(int) << std::endl;
-    std::cout << sizeof(uint8_t) << std::endl;
-    std::cout << sizeof(uint16_t) << std::endl;
-    std::cout << sizeof(uint32_t) << std::endl;
-    std::cout << sizeof(uint64_t) << std::endl;
     Game game;
     return 0;
 }
